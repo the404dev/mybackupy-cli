dd3fc0177d71d8f02fac20c2274e9a6b3f962f7a
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-10-06 16:26:16 -0300

    rename args compress_backup function

--- backupy2win/backup.py
+++ backupy2win/backup.py
@@ -3,19 +3,19 @@ from datetime import datetime
 class Backup:
     def __init__(self) -> None:
         pass
-        
 
-    def compress_7z_and_backup(type, filename, source_path, final_path):
-        if type == 'email':
-            print('closing Thunderbird')
+    def compress_backup(backup_type, zip_filename, source_file, detination_name_path):
+        if backup_type == 'email':
+            print('closing e-mail client')
             os.system('taskkill /f /im thunderbird.exe')
+            os.system('taskkill /f /im outlook.exe')
         
         os.chdir(r'C:\Backups')
 
         now = datetime.now()
         dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
 
-        with py7zr.SevenZipFile(f'{filename}_{dt_string}.7z', 'w', password=os.environ['ENCRYPT']) as archive:
+        with py7zr.SevenZipFile(f'{zip_filename}_{dt_string}.7z', 'w', password=os.environ['ENCRYPT']) as archive:
             print('Realizando backup...')
             print('Por favor aguarde...')
-            archive.writeall(source_path, final_path)
+            archive.writeall(source_file, detination_name_path)
