ed530eb1aae752a3a579f2874bca5af7703d5bc9
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-10-06 16:59:48 -0300

    add arg destination backup folder

--- backupy2win/backup.py
+++ backupy2win/backup.py
@@ -4,16 +4,15 @@ class Backup:
     def __init__(self) -> None:
         pass
 
-    def compress_backup(backup_type, zip_filename, source_file, detination_name_path):
+    def compress_backup(backup_type, zip_filename, source_file, detination_name_path_7zip, destination_backup_folder):
         if backup_type == 'email':
             print('closing e-mail client')
             os.system('taskkill /f /im thunderbird.exe')
             os.system('taskkill /f /im outlook.exe')
         
-        os.chdir(r'C:\Backups')
-
+        os.chdir(destination_backup_folder)
         datetime = Date.get_current_date_and_time()
         with py7zr.SevenZipFile(f'{zip_filename}_{datetime}.7z', 'w', password=os.environ['ENCRYPT']) as archive:
             print('Realizando backup...')
             print('Por favor aguarde...')
-            archive.writeall(source_file, detination_name_path)
+            archive.writeall(source_file, detination_name_path_7zip)
