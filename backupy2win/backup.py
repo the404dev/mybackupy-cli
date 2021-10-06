a3e6dbe619a8a330f816d53f2ac1d67beabe1b75
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-10-06 17:09:34 -0300

    separate process tasks

--- backupy2win/backup.py
+++ backupy2win/backup.py
@@ -1,15 +1,13 @@
 import os, py7zr
+from process import Process as proc
 from date import Date
+
 class Backup:
-    def __init__(self) -> None:
-        pass
 
     def compress_backup(backup_type, zip_filename, source_file, detination_name_path_7zip, destination_backup_folder):
         if backup_type == 'email':
-            print('closing e-mail client')
-            os.system('taskkill /f /im thunderbird.exe')
-            os.system('taskkill /f /im outlook.exe')
-        
+            proc.kill_process('thunderbird', 'outlook')
+            
         os.chdir(destination_backup_folder)
         datetime = Date.get_current_date_and_time()
         with py7zr.SevenZipFile(f'{zip_filename}_{datetime}.7z', 'w', password=os.environ['ENCRYPT']) as archive:
