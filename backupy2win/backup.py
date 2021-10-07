07bdca2e7fc4fbb5ae55ea987b8702f85631dbb8
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-10-07 11:46:29 -0300

    insert colorama into inputs and outputs messages

--- backupy2win/backup.py
+++ backupy2win/backup.py
@@ -1,6 +1,7 @@
+from colorama import Fore, Style, Back
+from date import Date
 import os, py7zr
 from process import Process as proc
-from date import Date
 
 class Backup:
 
@@ -11,23 +12,21 @@ class Backup:
         os.chdir(destination_backup_folder)
         datetime = Date.get_current_date_and_time()
         with py7zr.SevenZipFile(f'{zip_filename}_{datetime}.7z', 'w', password=password) as archive:
-            print('Comprimindo backup...')
-            print('Por favor aguarde...')
+            print(f'\n{Fore.YELLOW}Comprimindo backup... Por favor aguarde...\n')
             archive.writeall(source_file, detination_name_path_7zip)
-            print('compressão finalizada com sucesso!')
+            print(Fore.GREEN + 'compressão finalizada com sucesso!' + Style.RESET_ALL)
     
     def decompress_backup(source_backup_path, destination_backup_folder, password=None):
         try:
             archive = py7zr.SevenZipFile(source_backup_path, password=password)
-            print('Descomprimindo backup...')
-            print('Por favor aguarde...')
+            print(f'{Fore.YELLOW}Descomprimindo backup... Por favor aguarde...')
             archive.extractall(path=destination_backup_folder)
         except py7zr.exceptions.PasswordRequired:
-            print('Necessária senha para descompactar')
+            print(f'{Fore.RED}Necessária senha para descompactar')
             archive.close()
             return
         except:
-            print('Senha incorreta!')
+            print(f'{Fore.RED}Senha incorreta!')
             archive.close()
             return
         print('Finalizado com sucesso!')
--- backupy2win/cli.py
+++ backupy2win/cli.py
@@ -1,17 +1,21 @@
-from getpass import getpass
 from art import text2art
 from backup import Backup
+from colorama import Fore, Style
+from getpass import getpass
+import colorama
 
 
 class Cli:
     def __init__(self):
-        print(text2art('backupy2win'))
+        colorama.init(autoreset=True)
+        print(Fore.GREEN + text2art('backupy2win'))
         self.start_cli()
 
     def start_cli(self):
-        print('---------------------------------')
-        print('BEM VINDO AO UTILITÁRO DE BACKUP')
-        print('---------------------------------\n')
+        print(Fore.BLUE + '''
+                ---------------------------------------------
+                BEM VINDO AO UTILITÁRO DE BACKUP PARA WINDOWS
+                ---------------------------------------------\n''')
         self.init_menu()
 
     def init_menu(self):
@@ -19,19 +23,17 @@ class Cli:
     
     def create_backup_folder(self):
         print('Digite ou cole o endereço do diretorio para realizar o backup: ')
-        self.source_dir = input()
-        print(self.source_dir)
-        print('Digite ou cole o diretório de destino do backup: ')
-        self.destination_dir = input()
-        print(self.source_dir)
+        self.source_dir = input(Fore.BLUE)
+        print(f'{Style.RESET_ALL}Digite ou cole o diretório de destino do backup: ')
+        self.destination_dir = input(Fore.BLUE)
         self.insert_password()
         print('Digite um nome para o backup: ')
-        self.name_backup = input()
+        self.name_backup = input(Fore.BLUE)
         Backup.compress_backup('folder', self.name_backup, self.source_dir, self.name_backup, self.destination_dir, self.password)
 
     def insert_password(self):
         while True:
-            print('Digite a senha para backup - Pressione Enter para deixar em branco')
+            print(f'{Style.RESET_ALL}Digite a senha para backup - Pressione Enter para deixar em branco')
             self.password = getpass()
             if self.password == '':
                 self.password = None
@@ -39,7 +41,7 @@ class Cli:
             print('Digite novamente a senha para backup')
             self.repeat_password = getpass()
             if self.password != self.repeat_password:
-                print('Senhas não conferem!')
+                print(f'\n{Fore.RED}Senhas não conferem!\n')
             else:
                 return self.password
                 break
