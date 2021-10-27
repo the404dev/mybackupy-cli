9661d51e67b450ac6b0686da1baa2f0261462afb
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-10-27 08:15:12 -0300

    rename backup variables

--- src/backup.py
+++ src/backup.py
@@ -4,30 +4,30 @@ import os
 import py7zr
 
 class Backup:
-    def compress_backup(zip_filename, source_file, detination_name_path_7zip, destination_backup_folder, password=None):
+    def compress_backup(name, source_path, destination_path, password=None):
         try:
-            os.chdir(destination_backup_folder)
+            os.chdir(destination_path)
         except (FileNotFoundError, FileExistsError, OSError, Exception):
             print(f'{Fore.RED}Caminho não encontrado! Verifique o caminho digitado!')
             return
         datetime = Date.get_current_date_and_time()
                 
-        with py7zr.SevenZipFile(f'{zip_filename}_{datetime}.7z', 'w', password=password) as archive:
+        with py7zr.SevenZipFile(f'{name}_{datetime}.7z', 'w', password=password) as archive:
             print(f'\n{Fore.YELLOW}Comprimindo backup... Por favor aguarde...\n')
             try:
-                archive.writeall(source_file, detination_name_path_7zip)
+                archive.writeall(source_path, name)
                 print(Fore.GREEN + 'compressão finalizada com sucesso!' + Style.RESET_ALL)
             except FileNotFoundError:
                 print(f'{Fore.RED}Caminho não encontrado! Verifique o caminho digitado!')
                 return
 
 
-    def extract_backup(source_backup_path, destination_backup_folder, password=None):
+    def extract_backup(source_backup_path, destination_path, password=None):
         try:
             archive = py7zr.SevenZipFile(source_backup_path, password=password)
             try:
                 print(f'{Fore.YELLOW}Extraindo backup... Por favor aguarde...')
-                archive.extractall(path=destination_backup_folder)
+                archive.extractall(path=destination_path)
             
             except py7zr.exceptions.PasswordRequired:
                 print(f'{Fore.RED}Necessária senha para descompactar')
@@ -38,7 +38,7 @@ class Backup:
             finally:
                 archive.close()
             print('Finalizado com sucesso!')
-            print(f'Arquivo disponível em: {destination_backup_folder}')
+            print(f'Arquivo disponível em: {destination_path}')
         except FileNotFoundError:
             print(f'{Fore.RED}Caminho não encontrado! Verifique o caminho digitado!')
             return
