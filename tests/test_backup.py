51de10ce8f85790a487dfdb3518921ade33653c2
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-10-27 11:22:15 -0300

    add test compress and extract with password

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,5 +1,5 @@
 from src.backup import Backup
-from src.constants import TEST_NAME_BACKUP, TEST_SOURCE_PATH_BACKUP, TEST_DESTINATION_PATH_BACKUP
+from src.constants import TEST_NAME_BACKUP, TEST_SOURCE_PATH_BACKUP, TEST_DESTINATION_PATH_BACKUP, TEST_PASSWORD
 from unittest import TestCase
 
 class ProcessTest(TestCase):
@@ -30,5 +30,30 @@ class ProcessTest(TestCase):
 
         self.assertTrue(extract)
     
+    def test_compress_backup_with_password(self):
+        backup = Backup()
+        compress = backup.compress_backup(
+            TEST_NAME_BACKUP,
+            TEST_SOURCE_PATH_BACKUP,
+            TEST_DESTINATION_PATH_BACKUP,
+            TEST_PASSWORD
+        )
+              
+        self.assertTrue(compress)
+    
+
+    def test_extract_backup_with_password(self):
+        backup = Backup()
+
+        backup.compress_backup(
+            TEST_NAME_BACKUP,
+            TEST_SOURCE_PATH_BACKUP,
+            TEST_DESTINATION_PATH_BACKUP,
+            TEST_PASSWORD
+        )
+        extract = backup.extract_backup(backup.filename, TEST_DESTINATION_PATH_BACKUP, TEST_PASSWORD)
+
+        self.assertTrue(extract)
+
     def tearDown(self) -> None:
         return super().tearDown()
