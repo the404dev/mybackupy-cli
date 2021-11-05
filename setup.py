ea81c0dba9ae98d8c3a66ddf530e5924c5314fe7
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-11-05 15:12:39 -0300

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup
+import os
+
+version_file = open((f'{os.getcwd()}\VERSION'))
+version=version_file.read().strip()
 
 setup(
+    name='MyBackupy',
+    version=version,
+    description='Easily create and recover backups. MyBackupy compresses the contents of your folders into 7zip files for easy recovery.',
+    author='Cristian Penteado',
+    author_email='contato@cristianpdev.com',
+    url='https://github.com/cristianpdev/MyBackupy/',
     packages = [
         'src/backup.py',
         'src/cli.py',
