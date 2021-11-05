93b908fc603bf233c676e0f71203f3082cb08598
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-11-05 14:04:16 -0300

    migrate cli to argparse

--- src/cli.py
+++ src/cli.py
@@ -1,14 +1,12 @@
 from __future__ import print_function, unicode_literals
 from art import text2art
-from clint.textui import prompt, validators
 from src.backup import Backup
 from src.constants import THUNDERBIRD_PATH, OUTLOOK_PATH
-from getpass import getpass
-from src.process import kill_process
+import argparse
 
 class Cli:
     def __init__(self):
-        self.welcome()    
+        self.welcome()   
         self.start_cli()
     
 
@@ -16,86 +14,47 @@ class Cli:
         print(text2art('mybackupy'))
 
 
-    def start_cli(self):   
-        while True:     
-            print('Bem vindo ao utiliario de backup!\n')
-            response = self.main_menu()
-            if response == 'exit':
-                print('Finalizado.')
-                break
-            self.choise_menu_option(response)()
-            
-
-    def create_backup(self):
-        self.source_dir = self.ask_the_question_path('Digite ou cole o endereço do diretorio para realizar o backup: ')
-        self.destination_dir = self.ask_the_question_path('Digite ou cole o diretório de destino do backup: ')
-        self.password = self.insert_password()
-        self.name_backup = prompt.query('Digite um nome para o backup: ')
-        Backup.compress_backup(self, self.name_backup, self.source_dir, self.destination_dir, self.password)
+    def compress_backup(self, source):
+        Backup.compress_backup(self, self.args.name, source, self.args.destination, self.args.password)
     
+    def extract_backup(self):
+        Backup.extract_backup(self, self.args.source, self.args.destination, self.args.password)
 
-    def create_backup_email(self):
-        self.email_name = self.select_email()
-        self.destination_dir = self.ask_the_question_path('Digite ou cole o diretório de destino do backup: ')
-        self.password = self.insert_password()
-        self.name_backup = self.email_name
-        kill_process(self.name_backup)
-        if(self.name_backup == 'thunderbird'):
-            Backup.compress_backup(self, self.name_backup, THUNDERBIRD_PATH, self.destination_dir, self.password)
-        else:
-            Backup.compress_backup(self, self.name_backup, OUTLOOK_PATH, self.destination_dir, self.password)
+    def start_cli(self):
+        self.parser = argparse.ArgumentParser(prog="MyBackupy",description='Easily create and recover backups. MyBackupy compresses the contents of your folders into 7zip files for easy recovery.')
+        self.parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.2')
 
+        self.subparser = self.parser.add_subparsers(title="commands", dest='command')
+        self.backup = self.subparser.add_parser('backup_folder', add_help=True, description='Backup your folders')
+        self.extract = self.subparser.add_parser('extract_backup', add_help=True, description='Extract your backups back into folders')
+        self.email_thunderbird = self.subparser.add_parser('backup_thunderbird', add_help=True, description='Backup your Thunderbird emails')
+        self.email_outlook = self.subparser.add_parser('backup_outlook', add_help=True, description='Backup your Outlook emails')
 
-    def insert_password(self):
-        while True:
-            print('Digite a senha para backup - Pressione Enter para deixar em branco')
-            self.password = getpass()
-            if self.password == '':
-                self.password = None
-                return self.password
-            print('Digite novamente a senha para backup')
-            self.repeat_password = getpass()
-            if self.password != self.repeat_password:
-                print('\nSenhas não conferem!\n')
-            else:
-                return self.password
-    
+        self.backup.add_argument('-s', '--source', type=str, help='directory to compress and backup', required=True)
+        self.backup.add_argument('-d', '--destination', type=str, help='destination directory for backup', required=True)
+        self.backup.add_argument('-n', '--name', type=str, help='name for backup file', required=True)
+        self.backup.add_argument('-p', '--password', type=str, help='password to protect the backup file')
 
-    def extract_backup_cli(self):
-        self.source_backup = self.ask_the_question_path('Digite o endereço do backup incluindo o nome do arquivo: ')
-        self.destination_backup = self.ask_the_question_path('Digite o destino para extrair o backup:')
-        self.password = self.insert_password()
-        Backup.extract_backup(self, self.source_backup, self.destination_backup, self.password)
+        self.extract.add_argument('-s' ,'--source', type=str, help='full path including backup file name', required=True)
+        self.extract.add_argument('-d' ,'--destination', type=str, help='directory for backup extraction', required=True)
+        self.extract.add_argument('-p' ,'--password', type=str, help='password to extract the backup file', default=None)    
+
+        self.email_thunderbird.add_argument('-d','--destination', type=str, help='destination directory for Thunderbird backup', required=True)
+        self.email_thunderbird.add_argument('-n','--name', type=str, help='name for backup file', required=True)
+        self.email_thunderbird.add_argument('-p','--password', type=str, help='password to protect the backup file')
+
+        self.email_outlook.add_argument('-d','--destination', type=str, help='destination directory for Outlook backup', required=True)
+        self.email_outlook.add_argument('-n','--name', type=str, help='name for backup file', required=True)
+        self.email_outlook.add_argument('-p','--password', type=str, help='password to protect the backup file')
 
 
-    def choise_menu_option(self, option):
-        options = {
-            'folder_backup': self.create_backup,
-            'email_backup': self.create_backup_email,
-            'restore_backup': self.extract_backup_cli,
-        }
-        return options.get(option)
+        self.args = self.parser.parse_args()
+        self.check_args()
 
 
-    def ask_the_question_path(self, question):
-        return prompt.query(question, validators=[validators.PathValidator()])
-    
-
-    def main_menu(self):
-        inst_options = [
-                {'selector':'1','prompt':'Realizar backup de pasta','return':'folder_backup'},
-                {'selector':'2','prompt':'Realizar backup de e-mail','return':'email_backup'},
-                {'selector':'3','prompt':'Recuperar backup','return':'restore_backup'},
-                {'selector':'4','prompt':'Finalizar','return':'exit'}
-        ]
-
-        return prompt.options("Selecione uma opção", inst_options)
-
-
-    def select_email(self):
-        email_options = [
-                {'selector':'1','prompt':'Outlook','return':'outlook'},
-                {'selector':'2','prompt':'Thunderbird','return':'thunderbird'}
-        ]
-        return prompt.options("Selecione o cliente de e-mail", email_options)
-   
+    def check_args(self):
+        if self.args.command == 'backup_folder': self.compress_backup(self.args.source)
+        elif self.args.command == 'extract_backup': self.extract_backup()
+        elif self.args.command == 'backup_thunderbird': self.extract_backup(THUNDERBIRD_PATH)
+        elif self.args.command == 'backup_outlook': self.extract_backup(OUTLOOK_PATH)
+        else: self.parser.print_help()
