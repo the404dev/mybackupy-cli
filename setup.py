59be1a7ccf629ffab7d64b302792107cd68e172f
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-11-08 15:59:36 -0300

    update setup.py and fix version

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
-from setuptools import setup
-import os
-
-version_file = open((f'{os.getcwd()}\VERSION'))
-version=version_file.read().strip()
+from setuptools import setup, find_packages
 
 setup(
     name='MyBackupy',
-    version=version,
+    version='1.0.0b0',
     description='Easily create and recover backups. MyBackupy compresses the contents of your folders into 7zip files for easy recovery.',
     author='Cristian Penteado',
     author_email='contato@cristianpdev.com',
     url='https://github.com/cristianpdev/MyBackupy/',
-    packages = [
-        'src/backup.py',
-        'src/cli.py',
-        'src/constants.py'
-        'src/date.py'
-        'src/process.py'
-        ],
+    package_dir={"": "."},
+    packages=find_packages(where="src"),
     tests_require=['pytest'],
 )
