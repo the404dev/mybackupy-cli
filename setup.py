9e2e9f46602283430ca7c6edb79e563b3c9e006a
Cristian Penteado <cristianpenteado@protonmail.ch>
2021-11-09 10:08:41 -0300

    fix setup.py to build

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='MyBackupy',
+    name='mybackupy',
     version='1.0.0b0',
     description='Easily create and recover backups. MyBackupy compresses the contents of your folders into 7zip files for easy recovery.',
     author='Cristian Penteado',
     author_email='contato@cristianpdev.com',
     url='https://github.com/cristianpdev/MyBackupy/',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    py_modules=['mybackupy'],
     package_dir={"": "."},
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     console=['mybackupy.py'],
     entry_points = ''' 
         [console_scripts] 
-        mybackupy = mybackupy:main 
+        mybackupy=mybackupy:main 
     ''', 
     tests_require=['pytest'],
 )
